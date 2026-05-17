c19b6b455740dcf51467f3fb99a11261452763f1
SahaHungkul <mhasbysyabani27@gmail.com>
2026-05-17 21:21:40 +0700

    feat: add laravel breeze

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,20 @@
 <?php
 
-use App\Http\Controllers\V1\CategoryContoller;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\v1\ProductController;
-use App\Http\Controllers\v1\ShopController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::resource('categories', CategoryContoller::class);
-Route::resource('products', ProductController::class);
-Route::get('/shop', [ShopController::class, 'index'])->name('shop.index');
-Route::get('/shop/{product}', [ShopController::class, 'show'])->name('shop.show');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
